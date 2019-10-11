b072d68b9f6c30dfe0778f3103e215350907e68e
Nidvar <djayshine87@hotmail.com>
2019-10-11 14:39:28 +1100

    Updates

---
+++ precache-manifest.fdc393f82b5954901867b614578f1fa4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "974138eeeddc80ce9b2625e5dc3dd8b3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "df8c7ddee8a6624ead2a",
+    "url": "/portfolio/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "0a855af54c822da9e61a",
+    "url": "/portfolio/static/js/2.216f822f.chunk.js"
+  },
+  {
+    "revision": "df8c7ddee8a6624ead2a",
+    "url": "/portfolio/static/js/main.25521897.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  }
+]);
